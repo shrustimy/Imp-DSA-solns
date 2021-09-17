ab05f3a5320a0a7dab62141d319d46c541bb5804
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-17 23:53:46 +0530

    Add soln

---
+++ Max Consecutive Ones/prblm.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi=0,count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                count++;
+                if(count>maxi)
+                    maxi=count;
+            }
+            else
+            {
+                count=0;
+            }
+            
+        }
+        return maxi;
+        
+    }
+};
