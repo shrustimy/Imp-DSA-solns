f7fbdaaa91ba937acab78f72e9b508e1e615f901
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-19 20:31:28 +0530

    Add soln

---
+++ find duplicte number/soln.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+        int a;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                a=nums[i];
+                return nums[i]; 
+            }
+        }
+        return a;
+    }
+};
