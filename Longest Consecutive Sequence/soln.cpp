3998b2ac982a80b6afce81ac2648b9c2deff11b2
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-21 23:58:48 +0530

    Update soln.cpp

--- Longest Consecutive Sequence/soln.cpp
+++ Longest Consecutive Sequence/soln.cpp
@@ -6,7 +6,7 @@ public:
         if(nums.size()==0)
             return 0;
         sort(nums.begin(),nums.end());
-        int count=1,maxi=1; // there will be atleast one number if there are no consecutive numbers Ex: here [1,10,13,25] count                                           //will be 1
+        int count=1,maxi=1; // there will be atleast one number if there are no consecutive numbers Ex: here [1,10,13,25] count will be 1
         for(int i=0;i<nums.size()-1;i++)
         {
             // if there are any duplicates, they will be neglected and i value will be incremented
