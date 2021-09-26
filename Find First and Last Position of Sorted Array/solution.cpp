d8646c041faee025bd0095f520c7f6fae0f407ae
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-09-26 23:51:28 +0530

    Add solution

---
+++ Find First and Last Position of Sorted Array/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>::iterator lower,upper;
+        vector<int> nums1={-1,-1};
+        if(nums.size()==0)
+            return nums1;
+        lower=lower_bound(nums.begin(),nums.end(),target);
+        if(lower==nums.end() || *lower!=target )
+        {
+            return nums1;
+        }
+        else{
+            upper=upper_bound(nums.begin(),nums.end(),target);
+            nums1[0]=lower-nums.begin();
+            nums1[1]=upper-nums.begin()-1;
+        }
+        return nums1;
+    }
+};
