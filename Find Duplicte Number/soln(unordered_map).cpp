be5731a76978e80c90fd49b6f77a781ab801e43e
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2021-10-01 22:51:27 +0530

    add meth2

---
+++ Find Duplicte Number/soln(unordered_map).cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+       unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(++m[nums[i]]>1)
+            {
+                return nums[i];
+            }
+        }
+         return -1;
+    }
+};
