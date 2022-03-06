8ad375cecfebfb2abe33078997aa8a4d596ea247
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-03-07 02:01:23 +0530

    Update soln.cpp

--- Linked Lists/Delete Node in a Linked List/soln.cpp
+++ Linked Lists/Delete Node in a Linked List/soln.cpp
@@ -7,25 +7,3 @@ public:
 };
 
 
-
-//---------------------------------------------------------------------------
-
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        while(node->next!=NULL)
-        {
-            node->val=node->next->val;
-            if(node->next->next==NULL)
-            {
-                delete(node->next);
-                node->next=NULL;
-            }
-            else{
-            node=node->next;
-            }
-        }
-
-            
-    }
-};
