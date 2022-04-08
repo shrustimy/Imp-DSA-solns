b2e900be14b71a2eda456a4759e784ad2d35659e
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-04-08 12:08:55 +0530

    Update soln.cpp

--- Monthly Challenge/April/April8/soln.cpp
+++ Monthly Challenge/April/April8/soln.cpp
@@ -24,3 +24,22 @@ public:
         return nums1[nums1.size()-kth];
     }
 };
+
+#include<algorithm>
+class KthLargest {
+public:
+    vector<int> nums1;
+    int kth;
+    KthLargest(int k, vector<int>& nums) {
+        kth=k;
+        for(int i=0;i<nums.size();i++)
+        {
+            nums1.push_back(nums[i]);
+        }
+    }
+    int add(int val) {
+        nums1.push_back(val);
+        sort(nums1.begin(),nums1.end());
+        return nums1[nums1.size()-kth];
+    }
+};
