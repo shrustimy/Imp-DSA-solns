9748368166a687814dad706561e4361d824326d1
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-04-08 12:18:25 +0530

    Update soln2(priority queue).cpp

--- Monthly Challenge/April/April8/soln2(priority queue).cpp
+++ Monthly Challenge/April/April8/soln2(priority queue).cpp
@@ -6,6 +6,28 @@ public:
         si=k;
         for(int i=0;i<nums.size();i++)
         {
+            add(nums[i]);
+        }
+    }
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>si)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+
+
+
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int si;
+    KthLargest(int k, vector<int>& nums) {
+        si=k;
+        for(int i=0;i<nums.size();i++)
+        {
             pq.push(nums[i]);
         }
         while(pq.size()>k)
