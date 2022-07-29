bde812064a386eb1f70034167594e145a66568ab
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-07-30 02:28:03 +0530

    Update soln(iterative).cpp

--- Linked Lists/Reverse Linked List/soln(iterative).cpp
+++ Linked Lists/Reverse Linked List/soln(iterative).cpp
@@ -1,3 +1,5 @@
+// method 1
+
 class Solution {
 //Iterative
 public:
@@ -13,5 +15,28 @@ public:
             curr=next;
         }
         return prev;  
+    }
+};
+
+// method 2 here at the beginning itself next points to curr->next i.e head->next
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+            return head;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt=head->next;
+        
+        while(curr!=NULL)
+        {
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            if(nxt!=NULL)
+                nxt=nxt->next;
+        }
+      return prev;
     }
 };
