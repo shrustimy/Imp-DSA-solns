c840049e61e5fae3be1af24b6e00827a02d04f64
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-07-30 13:33:42 +0530

    Update soln1.cpp

--- Linked Lists/21. Merge Two Sorted Lists/soln1.cpp
+++ Linked Lists/21. Merge Two Sorted Lists/soln1.cpp
@@ -26,3 +26,45 @@ public:
         return dummy->next;
     }
 };
+
+
+// same method but bit different code
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL)
+            return l2;
+        else if(l2==NULL)
+            return l1;
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=NULL;
+        int cnt=0;
+        
+        while( l1 && l2 )
+        {
+            if(l1->val < l2->val){
+                dummy->next=new ListNode(l1->val);
+                cnt++;
+                if(cnt==1)
+                    temp=dummy->next;
+                l1=l1->next;
+            }
+            else
+            {
+                dummy->next=new ListNode(l2->val);
+                cnt++;
+                if(cnt==1)
+                    temp=dummy->next;
+                l2=l2->next;
+            }
+            dummy=dummy->next;
+                
+        }
+        if(l1)
+            dummy->next=l1;
+        else if(l2)
+            dummy->next=l2;
+        return temp;
+    }
+};
