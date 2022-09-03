dde5195442fec6c540ecd0af9d9282950b5b04b4
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-09-03 06:38:12 +0530

    Update soln(optimized).cpp

--- Arrays/Best Time to Buy and Sell Stock/soln(optimized).cpp
+++ Arrays/Best Time to Buy and Sell Stock/soln(optimized).cpp
@@ -18,3 +18,37 @@ public:
         return maximum;        
     }
 };
+
+
+// Or
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size(),minimum=INT_MAX,maximum=0;
+        for(int i=0;i<n;i++)
+        {
+            minimum=min(prices[i],minimum);
+            if(prices[i]>minimum)
+            {
+                maximum=max(maximum,prices[i]-minimum);
+            }
+        }
+        return maximum;        
+    }
+};
+
+// Or
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size(),minimum=INT_MAX,maximum=0;
+        for(int i=0;i<n;i++)
+        {
+            minimum=min(prices[i],minimum);
+            maximum=max(maximum,prices[i]-minimum);
+        }
+        return maximum;        
+    }
+};
