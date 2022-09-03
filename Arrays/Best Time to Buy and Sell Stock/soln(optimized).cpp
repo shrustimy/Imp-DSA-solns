2ec8e28f88050cafec1d6621df958082a46f5be7
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-09-03 06:18:11 +0530

    Create soln(optimized).cpp

---
+++ Arrays/Best Time to Buy and Sell Stock/soln(optimized).cpp
@@ -0,0 +1,20 @@
+// Time complexity O(n)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size(),minimum=INT_MAX,maximum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(prices[i]<minimum)
+            {
+                minimum=prices[i];
+            }
+            if(prices[i]>minimum)
+            {
+                maximum=max(maximum,prices[i]-minimum);
+            }
+        }
+        return maximum;        
+    }
+};
