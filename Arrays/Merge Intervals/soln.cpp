17af8c10c99ad55b56f3903544190cf08a6d7867
Shrusti M Yaligar <71809335+shrustimy@users.noreply.github.com>
2022-09-11 12:36:35 +0530

    Update soln.cpp

--- Arrays/Merge Intervals/soln.cpp
+++ Arrays/Merge Intervals/soln.cpp
@@ -1,11 +1,7 @@
 class Solution {
-public:
-    static bool sortcol(const vector<int>& v1,const vector<int>& v2) { 
-     return v1[0] < v2[0]; 
-    }
-    
+public:    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(),sortcol);
+        sort(intervals.begin(), intervals.end());
         for(int i=0;i<intervals.size()-1;i++)
         {
             if((intervals[i][1]>=intervals[i+1][0]))
